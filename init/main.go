313863ac8003cea848a873026567edc3c1c1ddcd
KoorunGG <onlyzion10@naver.com>
2024-04-07 00:15:20 +0900

    add: http 내장 패키지를 이용한 go 서버를 여는 코드 작성

--- init/main.go
+++ init/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", helloWorld)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("에러 발생")
+		log.Fatalln(err)
+	}
+}
+
+func helloWorld(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("hello world")
 }
